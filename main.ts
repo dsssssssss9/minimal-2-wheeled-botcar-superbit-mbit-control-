a1b0d3aae5b75dfefeaaf7f7b83913699a63763d
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2025-01-01 16:30:23 +0000

    add announcer config

--- main.ts
+++ main.ts
@@ -36,6 +36,26 @@ function bluecontrol () {
         )
     }
 }
+function Set_Volume () {
+    let l = control.createBuffer(7)
+l.setUint8(0, 0x7E)
+l.setUint8(1, 0x06)
+l.setUint8(2, 0x00)
+l.setUint8(3, 0x02)
+l.setUint8(4, 0x00)
+l.setUint8(5, Volume)
+l.setUint8(6, 0xEF)
+serial.writeBuffer(l)
+}
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Happy)
+    connected = 1
+    while (connected == 1) {
+        uartData = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
+        bluecontrol()
+        ModeSelect()
+    }
+})
 bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Sad)
     connected = 0
@@ -55,21 +75,30 @@ function ModeSelect () {
         g_mode = 0
     }
 }
-bluetooth.onBluetoothConnected(function () {
-    basic.showIcon(IconNames.Happy)
-    connected = 1
-    while (connected == 1) {
-        uartData = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
-        bluecontrol()
-        ModeSelect()
-    }
-})
+function Play_Track () {
+    let m = control.createBuffer(7)
+m.setUint8(0, 0x7E)
+m.setUint8(1, 0x03)
+m.setUint8(2, 0x00)
+m.setUint8(3, 0x02)
+m.setUint8(4, 0x00)
+m.setUint8(5, Track_Num)
+m.setUint8(6, 0xEF)
+serial.writeBuffer(m)
+}
 let g_mode = 0
 let uartData = ""
 let connected = 0
+let Track_Num = 0
+serial.redirect(
+SerialPin.P2,
+SerialPin.P1,
+BaudRate.BaudRate9600
+)
+let Volume = 16
 connected = 0
 bluetooth.startUartService()
-basic.showString("#")
+basic.showString("J")
 basic.forever(function () {
 	
 })
